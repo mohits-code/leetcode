d308c570057dc63ce7c368fe8b2766999a05851f
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2024-06-25 14:07:41 -0400

    Time: 11 ms (91.82%) | Memory: 11.7 MB (6.13%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,18 +1,16 @@
 class Solution(object):
     def removeElement(self, nums, val):
         
-        t=len(nums)
 
         if val in nums:
             nums.sort()
             i = nums.index(val)
             while nums[i]==val:
-                t-=1
                 nums.pop(i)
                 if i==len(nums):
                     break
         
-        return t
+        return len(nums)
         
         
         '''
