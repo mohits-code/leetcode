9a23229d4c510f01ac17c5318ead29c9eefafa2a
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2024-07-02 11:58:45 -0400

    Time: 30 ms (56.52%) | Memory: 11.5 MB (76.70%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -7,11 +7,11 @@ class Solution(object):
         d={"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
         total = 0
         l=len(s)
-        for i, char in enumerate(s):
-            c=d[char]
+        for i in range(len(s)-1):
+            c=d[s[i]]
             if i+1<l and c< d[s[i+1]]:
                 total-=c
             else: 
                 total+=c
 
-        return total
+        return total+d[s[-1]]
