2ac5266aaac052511c30a8d49eb5ce18340eba4f
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2024-07-02 13:13:18 -0400

    Time: 15 ms (98.56%) | Memory: 11.59 MB (63.53%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.py
+++ 12-integer-to-roman/integer-to-roman.py
@@ -1,9 +1,5 @@
 class Solution(object):
     def intToRoman(self, num):
-        """
-        :type num: int
-        :rtype: str
-        """
         s=""
         d = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
         x=1
