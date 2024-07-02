749711c584fbd985d8570f3a53f2edf92dd94979
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2024-07-02 13:08:21 -0400

    Time: 32 ms (50.12%) | Memory: 11.7 MB (27.69%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        s=""
+        d = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+        x=1
+        while num>0:
+            n=num%10
+            if n==4:
+                s=d[x]+d[x*5]+s
+            elif n==9:
+                s=d[x]+d[x*10]+s
+            else:
+                if n>=5:
+                    n-=5
+                    s=d[x]*n+s
+                    s=d[x*5]+s
+                else:
+                    s=d[x]*n+s
+            x*=10
+            num//=10
+        return s
+
+                    
+
+
+        
