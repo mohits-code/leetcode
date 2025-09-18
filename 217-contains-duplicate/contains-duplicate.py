5a3fb72b93a8a2603e8fe8804fa9c9c97e9b1833
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2025-09-18 17:29:57 -0400

    Time: 88 ms (6.24%) | Memory: 20 MB (97.46%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,5 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        return len(set(nums))!=len(nums)
+        nums.sort()
+
+        for i in range(len(nums)-1):
+            if nums[i]==nums[i+1]:
+                return True
+        
+        return False
+        
         
