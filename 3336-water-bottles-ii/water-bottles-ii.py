20b73fcdf8cdc2d722cad59f5b2534cd7cf36ecf
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2025-10-02 16:45:46 -0400

    Time: 50 ms (5.38%) | Memory: 17.7 MB (68.56%) - LeetSync

--- 3336-water-bottles-ii/water-bottles-ii.py
+++ 3336-water-bottles-ii/water-bottles-ii.py
@@ -1,15 +1,7 @@
 class Solution:
     def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
-        total=0
-        emptyBottles=0
-
-        while numBottles !=0 or emptyBottles>numExchange:
-            total+=numBottles
-            emptyBottles+=numBottles
-            numBottles=0
-            while emptyBottles>=numExchange:
-                numBottles+=1
-                emptyBottles-=numExchange
-                numExchange+=1
-
-        return total
+        if numBottles<numExchange: return numBottles
+        b=2*numExchange-3
+        c=-2*numBottles
+        cost = int((-1*b +  math.sqrt(b**2 - 4*c))-0.000000001)//2
+        return numBottles+cost
