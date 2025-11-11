9fa32b5055f6c319e44f8f2613a7538219fb7d45
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2025-11-10 23:13:50 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (48.37%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        d={}
+        def recur(n):
+            if n<=3:
+                return n
+            else:
+                if n in d:
+                    return d[n]
+                d[n-1]=recur(n-1)
+                return recur(n-1)+recur(n-2)
+
+        return recur(n)
