c35f42bfa1aa5ff2b1ffc73ccdcc450f930e9ec9
Mohit Singh <107310472+mohits-code@users.noreply.github.com>
2026-07-19 21:02:10 -0700

    Time: 110 ms (73.69%) | Memory: 77.2 MB (46.23%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,28 +1,61 @@
 class LRUCache:
+    class deque:
+        class Node:
+            def __init__(self, key, val):
+                self.key = key
+                self.val = val
+                self.next = None
+                self.prev = None
+        
+        def __init__(self):
+            self.head = self.Node(0,0)
+            self.tail = self.Node(0,0)
+            self.head.prev = self.tail
+            self.tail.next = self.head
+        
+        def insert(self, node):
+            prev_node = self.head.prev
+            prev_node.next = node
+            self.head.prev = node
+            node.prev = prev_node
+            node.next = self.head
+        
+        def remove(self, node):
+            prev_node = node.prev
+            next_node = node.next
+            prev_node.next = next_node
+            next_node.prev = prev_node
 
     def __init__(self, capacity: int):
         self.cap = capacity
-        self.queue = []
         self.dict = {}
+        self.dll = self.deque()
 
     def get(self, key: int) -> int:
         if key in self.dict:
-            self.queue.remove(key)
-            self.queue.append(key)
-            return self.dict[key]
+            node = self.dict[key]
+            self.dll.remove(node)
+            self.dll.insert(node)
+            return node.val
         return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.dict:
-            self.dict[key] = value
-            self.queue.remove(key)
-            self.queue.append(key)
+            self.dict[key].val = value
+            node = self.dict[key]
+            self.dll.remove(node)
+            self.dll.insert(node)
             return
-        elif self.cap == 0:
-            del self.dict[self.queue.pop(0)]
+        
+        if self.cap == 0:
+            lru_node = self.dll.tail.next
+            self.dll.remove(lru_node)
+            del self.dict[lru_node.key]
             self.cap += 1
-        self.dict[key] = value
-        self.queue.append(key)
+
+        new_node = self.deque.Node(key, value)
+        self.dict[key] = new_node
+        self.dll.insert(new_node)
         self.cap -= 1
 
 
